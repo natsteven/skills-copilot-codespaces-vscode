6cd98290af7e205cd8ca81c4a64d266aca09b2b3
nat steven <nathanaelsteven@gmail.com>
2023-11-04 23:28:53 +0000

    thid commit

--- comments.js
+++ comments.js
@@ -1,1 +1,7 @@
 // Create a web server
+
+const express = require('express');
+const router = expressRouter();
+const commentController = require('../controllers/comment')
+
+router.get('/', commentController.comment_list);
